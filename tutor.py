dbaca061eb888d444275b71f6db1db9d05f33182
marianalaranjo <mariana.laranjo@tecnico.ulisboa.pt>
2024-06-08 15:34:25 +0100

    correct dependencies v2

--- tutor.py
+++ tutor.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import json
 import random
-from langchain_openai.chat_models.base import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain.callbacks.manager import CallbackManager
