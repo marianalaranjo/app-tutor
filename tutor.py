60de485d251e3fe518b54dcb5bb2f6c49b929396
marianalaranjo <mariana.laranjo@tecnico.ulisboa.pt>
2024-06-09 17:13:17 +0100

    correct logs v23

--- tutor.py
+++ tutor.py
@@ -15,8 +15,8 @@ from google.oauth2.service_account import Credentials
 
 ## CONFIG
 
-#key_dict = json.loads(st.secrets["textkey"])
-creds = Credentials.from_service_account_info(st.secrets["textkey"])
+key_dict = json.loads(st.secrets["textkey"])
+creds = Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="app-tutor")
 
 TUTOR_MODEL = random.choice([True, False])
