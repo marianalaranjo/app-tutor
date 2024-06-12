cd2a8084d59d327301e56040db255bfd963664b5
marianalaranjo <mariana.laranjo@tecnico.ulisboa.pt>
2024-06-12 12:50:21 +0100

    perguntas finais v7

--- tutor.py
+++ tutor.py
@@ -12,13 +12,14 @@ from google.cloud import firestore
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import initialize_app
 
 ## CONFIG
 
 key_dict = json.loads(st.secrets["textkey"])
 cred = credentials.Certificate(key_dict)
 
-firebase_admin.initialize_app(cred)
+app= initialize_app(cred)
 
 db = firestore.client()
 
